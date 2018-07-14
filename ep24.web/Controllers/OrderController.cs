a439fe3bbb118d291677f0c8e4d249c3e85cb564
Kritsada Dechawantana <jokerstudio@hotmail.com>
2018-07-14 10:38:28 +0700

    fix listhistory and orderproduct error case

--- ep24.web/Controllers/OrderController.cs
+++ ep24.web/Controllers/OrderController.cs
@@ -24,39 +24,47 @@ namespace ep24.web.Controllers
         [HttpGet]
         public IEnumerable<Order> ListHistory()
         {
-            //TODO: implement scenario ขอรายการสั่งซื้อที่ยืนยันรายการสั่งซื้อแล้ว
-            throw new NotImplementedException();
+            var histories = orderRepo.List(it => it.PaidDate.HasValue).ToList();
+            return histories;
         }
 
         [HttpPost]
         public OrderProductResponse OrderProduct([FromBody]OrderProductRequest request)
         {
-            //TODO: implement scenario ไม่มีข้อมูล หรือไม่เลือกสินค้าที่จะสั่ง ให้แจ้งกลับว่า 'ไม่พบเมนูที่จะสั่ง' และไม่บันทึกข้อมูล
-
+            if (request == null || request.OrderedProducts == null) return new OrderProductResponse { Message = "ไม่พบเมนูที่จะสั่ง" };
+            var hasOrder = request.OrderedProducts.Any();
             var productIds = request.OrderedProducts.Select(p => p.Key);
             var products = productRepo.GetAllProducts();
-            var filteredProducts = products.Where(p => productIds.Contains(p.Id)).ToList();
-            if (filteredProducts.Count() != productIds.Count())
-            {
-                return new OrderProductResponse { Message = "ไม่พบสินค้าบางรายการ กรุณาสั่งใหม่อีกครั้ง", };
-            }
+            var hasProducts = products.Any();
+            var canOrder = hasOrder || hasProducts;
+            if (canOrder){
+                var filteredProducts = products.Where(p => productIds.Contains(p.Id)).ToList();
+                if (filteredProducts.Count() != productIds.Count())
+                {
+                    return new OrderProductResponse { Message = "ไม่พบสินค้าบางรายการ กรุณาสั่งใหม่อีกครั้ง", };
+                }
 
-            if (filteredProducts.Any(p => p.HasStock && p.Stock < request.OrderedProducts.First(op => op.Key == p.Id).Value))
+                if (filteredProducts.Any(p => p.HasStock && p.Stock < request.OrderedProducts.First(op => op.Key == p.Id).Value))
+                {
+                    return new OrderProductResponse { Message = "สินค้าบางรายการมีไม่พอ กรุณาสั่งใหม่อีกครั้ง", };
+                }
+                var id = Guid.NewGuid().ToString();
+                var order = new Order
+                {
+                    Id = id,
+                    OrderedProducts = filteredProducts,
+                    ReferenceCode = id.Substring(0, 5),
+                    OrderDate = DateTime.UtcNow,
+                    Username = request.Username,
+                };
+                orderRepo.Create(order);
+                
+                return new OrderProductResponse { Message = "สั่งเมนูสำเร็จ กรุณาชำระเงินที่เค้าเตอร์", ReferenceCode = order.ReferenceCode, };
+            } 
+            else
             {
-                return new OrderProductResponse { Message = "สินค้าบางรายการมีไม่พอ กรุณาสั่งใหม่อีกครั้ง", };
+                return new OrderProductResponse { Message = "ไม่พบเมนูที่จะสั่ง" };
             }
-            var id = Guid.NewGuid().ToString();
-            var order = new Order
-            {
-                Id = id,
-                OrderedProducts = filteredProducts,
-                ReferenceCode = id.Substring(0, 5),
-                OrderDate = DateTime.UtcNow,
-                Username = request.Username,
-            };
-            orderRepo.Create(order);
-            
-            return new OrderProductResponse { Message = "สั่งเมนูสำเร็จ กรุณาชำระเงินที่เค้าเตอร์", ReferenceCode = order.ReferenceCode, };
         }
     }
 }
